e9ea4aa8874e20c19d06fcdf2f85258aa4b8613a
Ry2uko <gabrielfradz@hotmail.com>
2025-03-21 15:23:04 +0800

    uwi na q

--- frontend/src/components/Display/Display.jsx
+++ frontend/src/components/Display/Display.jsx
@@ -4,6 +4,7 @@ import './Display.css';
 
 const Display = ({ displayType='content', displayData }) => {
     const [searchVal, setSearchVal] = useState('');
+    const [kebabStatus, setKebabStatus] = useState({ 1: true });
     const { loginType } = useContext(LoginContext);
 
     // Display types: content, student, classroom
@@ -31,16 +32,50 @@ const Display = ({ displayType='content', displayData }) => {
                 <div className="display-header">
                     <div className="filters-container">
                         <div className="filter selected">All</div>
-                        <div clasSname="filter">Filter A</div>
-                        <div clasSname="filter">Filter B</div>
-                        <div clasSname="filter">Filter C</div>
+                        <div className="filter">Filter A</div>
+                        <div className="filter">Filter B</div>
+                        <div className="filter">Filter C</div>
                     </div>
-                    <button type="button">
-                        <i className="fa-solid fa-plus"></i>
-                    </button>
+                    {
+                        loginType !== 'student' && (
+                            <button type="button">
+                                <i className="fa-solid fa-plus"></i>
+                            </button>
+                        )
+                    }
                 </div>
                 <div className="display-body">
-
+                    <div className="content-items-container">
+                        <div className="content-item" key="1">
+                            <div className="content-icon">
+                                <i className="fa-solid fa-book"></i>
+                            </div>
+                            <div className="content-details">
+                                <span className="content-title">
+                                    Creative Portfolio
+                                </span>
+                                <span className="details-group">
+                                    <small className="content-date">
+                                        March 21, 2025
+                                    </small>
+                                </span>
+                            </div>
+                            <button type="button" className="kebab">
+                                <i className="fa-solid fa-ellipsis-vertical"></i>
+                            </button>
+                            { kebabStatus[1] && (
+                                <div className="kebab-container">
+                                    <ul>
+                                        <li>Copy Link</li>
+                                        <li>Edit Content</li>
+                                    </ul>
+                                </div>
+                            ) }
+                        </div>
+                    </div>
+                    <div className="course-items-container">
+                        <div className="course-item"></div>
+                    </div>
                 </div>
             </div>
         </div>
